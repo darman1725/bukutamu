ac1de7375bbb3a25511a25b3c4937abcf7bfb814
darman1725 <darmansaragih1701@gmail.com>
2022-07-02 15:53:20 +0700

    Dashboard Admin Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\User\TamuController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,7 @@ Route::get('/', function () {
 });
 
 Route::post('simpan-bukutamu', [TamuController::class, 'simpanTamu'])->name('simpan-bukutamu');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
